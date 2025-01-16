69e3554f6aba5c02da640efd85567ec47e58e89c
rat <anna.danilenko.06@mail.ru>
2025-01-16 23:57:31 +0500

    исправленная лабораторная

--- lab1_fixed.py
+++ lab1_fixed.py
@@ -1,76 +1,76 @@
-# # Задание №1
-# def draw_swiss_flag():
-#     flag_width = 10
-#     flag_height = 5
+# Задание №1
+def draw_swiss_flag():
+    flag_width = 10
+    flag_height = 5
     
-#     RED = '\033[41m  \033[0m'   
-#     WHITE = '\033[47m  \033[0m'   
+    RED = '\033[41m  \033[0m'   
+    WHITE = '\033[47m  \033[0m'   
 
-#     for i in range(flag_height):
-#         if i == 0:   
-#             print(RED * flag_width)
-#         elif i == 1:  
-#             print(RED * 4 + WHITE * 2 + RED * 4)
-#         elif i == 2:  
-#             print(RED * 2 + WHITE * 6 + RED * 2)
-#         elif i == 3:
-#             print(RED * 4 + WHITE * 2 + RED * 4)
-#         elif i == 4:  
-#             print(RED * flag_width)
+    for i in range(flag_height):
+        if i == 0:   
+            print(RED * flag_width)
+        elif i == 1:  
+            print(RED * 4 + WHITE * 2 + RED * 4)
+        elif i == 2:  
+            print(RED * 2 + WHITE * 6 + RED * 2)
+        elif i == 3:
+            print(RED * 4 + WHITE * 2 + RED * 4)
+        elif i == 4:  
+            print(RED * flag_width)
 
-# if name=='main':
-#     draw_swiss_flag() 
+if __name__=='__main__':
+    draw_swiss_flag() 
 
  
  
-# # Задание №2
-# def draw_line(lenght = 1,offset = 0, gap = 0, whitespacel = 0):
-#     line = " " * lenght
-#     print(
-#         f'{" " * offset}'
-#         f'\x1b[48;5;255m{line}\x1b[0m{" " * gap}'
-#         f'\x1b[48;5;255m{line}\x1b[0m{" " * whitespacel}'
-#         f'\x1b[48;5;255m{line}\x1b[0m{" " * gap}'
-#         f'\x1b[48;5;255m{line}\x1b[0m'
-#     )
+# Задание №2
+def draw_line(lenght = 1,offset = 0, gap = 0, whitespacel = 0):
+    line = " " * lenght
+    print(
+        f'{" " * offset}'
+        f'\x1b[48;5;255m{line}\x1b[0m{" " * gap}'
+        f'\x1b[48;5;255m{line}\x1b[0m{" " * whitespacel}'
+        f'\x1b[48;5;255m{line}\x1b[0m{" " * gap}'
+        f'\x1b[48;5;255m{line}\x1b[0m'
+    )
 
-# def circle():
-#     height = 8
-#     step = 2
-#     lenght = 2
-#     offset = 15
-#     whitespacel = 12
-#     gap = height * 2 - step * lenght
-#     center = height / 2
-#     for line in range(height + 1):
-#         if line == 0:
-#             draw_line(height, offset,  0, whitespacel)
-#         elif line < center:
-#             offset -= step
-#             whitespacel -= step * 2
-#             gap += step * lenght 
-#             draw_line(lenght, offset, gap, whitespacel)
-#         elif line == center or line == center + 1:
-#             #gap += step
-#             draw_line(lenght, offset, gap, whitespacel)
-#         elif line == height:
-#             offset += step
-#             whitespacel += step * 2 
-#             draw_line(height, offset, 0, whitespacel)
-#         else:
-#             gap -= step * lenght
-#             offset += step
-#             whitespacel += step * 2
-#             draw_line(lenght, offset, gap, whitespacel)
-
-
-# if name=='main':
-#     circle() 
+def circle():
+    height = 8
+    step = 2
+    lenght = 2
+    offset = 15
+    whitespacel = 12
+    gap = height * 2 - step * lenght
+    center = height / 2
+    for line in range(height + 1):
+        if line == 0:
+            draw_line(height, offset,  0, whitespacel)
+        elif line < center:
+            offset -= step
+            whitespacel -= step * 2
+            gap += step * lenght 
+            draw_line(lenght, offset, gap, whitespacel)
+        elif line == center or line == center + 1:
+            #gap += step
+            draw_line(lenght, offset, gap, whitespacel)
+        elif line == height:
+            offset += step
+            whitespacel += step * 2 
+            draw_line(height, offset, 0, whitespacel)
+        else:
+            gap -= step * lenght
+            offset += step
+            whitespacel += step * 2
+            draw_line(lenght, offset, gap, whitespacel)
 
 
-# #Задание №3
-# for x in range(28, 0, -3):
-#     print (f'{" " * x * 3}\x1b[48;5;255m{" " * 3 * 3}\x1b[0m')
+if __name__=='__main__':
+    circle() 
+
+
+#Задание №3
+for x in range(28, 0, -3):
+    print (f'{" " * x * 3}\x1b[48;5;255m{" " * 3 * 3}\x1b[0m')
 
 
 #Задание №4
